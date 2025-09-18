7d399cceecbe605e90390a772c46ff039de45289
alya <alyanasmi7@gmail.com>
2025-09-18 09:32:58 +0700

    update placehikder

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,7 +19,7 @@ export default function ContactPage() {
               type="text"
               id="name"
               name="name"
-              placeholder="Masukkan nama"
+              placeholder="Masukkan namamu"
               required
               className="w-full px-4 py-2 rounded bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-500"
             />
